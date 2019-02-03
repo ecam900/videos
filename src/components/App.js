fb3ba024f2cf5e5e17af9921edcd4cb0ba05001b
Eric Crescioni <ecam900@gmail.com>
2019-02-03 15:08:09 -0400

    Set state on App component. Create GET request for videos, via axios.

--- src/components/App.js
+++ src/components/App.js
@@ -6,19 +6,24 @@ import youtube from '../apis/youtube';
 
 
 class App extends React.Component {
+  state = { videos: [] };
 
-  onTermSubmit = (term) => {
-    youtube.get('/search', {
+  onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
       params: {
         q: term
       }
     });
-  }
+
+
+    this.setState({ videos: response.data.items })
+  };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        I have {this.state.videos.length} videos for you!
       </div>
     );
   }
