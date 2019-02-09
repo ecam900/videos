884a3ca8f0389a69d16aaf5e7acd3566bfe46226
Eric Crescioni <ecam900@gmail.com>
2019-02-09 15:09:20 -0400

    Create VideoList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
 
 
 
@@ -23,7 +24,7 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        I have {this.state.videos.length} videos for you!
+        <VideoList />
       </div>
     );
   }
